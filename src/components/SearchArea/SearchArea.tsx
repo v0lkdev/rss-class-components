e60011f12c9067d095c1633dc4056b99f7ffb488
v0lkdev <bo04092001@gmail.com>
2026-05-03 23:38:42 +0200

    feat: implement search with local storage

--- src/components/SearchArea/SearchArea.tsx
+++ src/components/SearchArea/SearchArea.tsx
@@ -1,11 +1,29 @@
 import { Component } from "react";
+import type { ChangeEvent } from 'react';
 import './SearchArea.css';
 
 export default class SearchArea extends Component {
+    state = {
+        inputInitialValue: localStorage.getItem('currentSearchValue') || '',
+    }
+
+    handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        this.setState({
+            inputInitialValue: e.target.value
+        });
+        localStorage.setItem('currentSearchValue',  e.target.value);
+    }
+
     render() {
         return (
             <div className="search-area">
-                <input type="text" className="search-input" placeholder="Type book name..."/>
+                <input
+                    type="text" 
+                    className="search-input"
+                    placeholder="Type book name..." 
+                    value={this.state.inputInitialValue}
+                    onChange={this.handleInputChange}
+                />
                 <button className="search-btn">Search</button>
             </div>
         )
