e3b7cd7ef1bc3275fd7260a2d67436a9a9f2a170
v0lkdev <bo04092001@gmail.com>
2026-05-04 12:19:05 +0200

    feat: add search results display

--- src/components/ResultArea/ResultArea.tsx
+++ src/components/ResultArea/ResultArea.tsx
@@ -1,19 +1,28 @@
 import { Component } from "react";
 import './ResultArea.css'
+import ResultItem from "./ResultItem/Resultem";
+import ResultItemDescription from "./ResultItemDescription/ResultItemDescription";
 
 export default class ResultArea extends Component {
+
+
     render() {
         return (
             <div className="result-area">
-                <div className="item-name-wrapper">
+                <div className="item-title-wrapper">
                     <div className="list-header">Book name</div>
-                    <ol className="item-name"></ol>
+                    <ol className="item-title-list"> 
+                        <ResultItem title="Oliver Twist"/>  
+                        <ResultItem title="Oliver Twist"/>  
+                    </ol>
                 </div>
                 <div className="item-description-wrapper">
                     <div className="list-header">Book Description</div>
-                    <ol className="item-description">
+                    <ol className="item-description-list">
+                        <ResultItemDescription author='Charles Dickens' publishYear={1822} editionCount={2209}/> 
+                        <ResultItemDescription author='Charles Dickens' publishYear={1822} editionCount={2209}/>  
                     </ol>
-                </div>
+                </div>  
             </div>
         )
     }
